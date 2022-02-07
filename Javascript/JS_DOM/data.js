8b49a4c7890ae1d308929030c1c4d7b8075cc41d
vmurali100 <vmurali100@gmail.com>
2022-02-07 19:41:10 +0530

    Adding Dynamic Tables

---
+++ Javascript/JS_DOM/data.js
@@ -0,0 +1,92 @@
+var users = [
+    {
+        "fname": "Luciana",
+        "lname": "Haugland",
+        "tel": "(253)976-3769",
+        "address": "6237 Ipsum Ct",
+        "city": "Baton Rouge",
+        "state": "DE",
+        "zip": 23784
+    },
+    {
+        "fname": "Arzak",
+        "lname": "Delaura",
+        "tel": "(289)269-7705",
+        "address": "5247 Sollicitudin Dr",
+        "city": "Oklahoma",
+        "state": "SD",
+        "zip": 79173
+    },
+    {
+        "fname": "Gene",
+        "lname": "Kilpatrick",
+        "tel": "(394)073-5852",
+        "address": "1870 Lacus Ave",
+        "city": "St Augustine",
+        "state": "CT",
+        "zip": 82851
+    },
+    {
+        "fname": "Barbara",
+        "lname": "Walstead",
+        "tel": "(556)929-3680",
+        "address": "3808 Orci Ln",
+        "city": "Brighton",
+        "state": "NC",
+        "zip": 61731
+    },
+    {
+        "fname": "Elisha",
+        "lname": "Schade",
+        "tel": "(915)808-0757",
+        "address": "2744 Fringilla St",
+        "city": "Carrollton",
+        "state": "FL",
+        "zip": 66303
+    },
+    {
+        "fname": "Kristy",
+        "lname": "Grund",
+        "tel": "(758)062-4288",
+        "address": "7520 Elit Ln",
+        "city": "Pharr",
+        "state": "NY",
+        "zip": 30699
+    },
+    {
+        "fname": "Gladys",
+        "lname": "Wesley",
+        "tel": "(397)565-8731",
+        "address": "8572 Vitae Ln",
+        "city": "Lisle",
+        "state": "PA",
+        "zip": 49169
+    },
+    {
+        "fname": "Dalton",
+        "lname": "Holliday",
+        "tel": "(165)182-2157",
+        "address": "7578 Etiam Ct",
+        "city": "Valparaiso",
+        "state": "MN",
+        "zip": 23830
+    },
+    {
+        "fname": "Geraldine",
+        "lname": "Kasnow",
+        "tel": "(941)554-0012",
+        "address": "2982 Tortor Rd",
+        "city": "Calgary",
+        "state": "WV",
+        "zip": 32596
+    },
+    {
+        "fname": "Carmen",
+        "lname": "Diriwachter",
+        "tel": "(263)788-1493",
+        "address": "6970 Nullam Ln",
+        "city": "Brooklyn Park",
+        "state": "FL",
+        "zip": 64624
+    }
+]
