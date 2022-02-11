71c12e32deea594b6d6f2aa635552b644e06ceea
Mkunal18 <kunal.maingade@gmail.com>
2022-02-11 20:11:07 +0530

    hello

---
+++ javascript/Elements/details.js
@@ -0,0 +1,8 @@
+var users = [{ id: 91, fname: "Ramesh", lname: "Singh", age: 29, phonenumber: 9898989898, state: "Maharashtra", city: "Mumbai", zip: 400054 },
+{ id: 92, fname: "Suresh", lname: "Patil", age: 33, phonenumber: 8989898989, state: "Maharashtra", city: "Mumbai", zip: 400055 },
+{ id: 93, fname: "Mahesh", lname: "Gawade", age: 35, phonenumber: 7979797979, state: "Maharashtra", city: "Mumbai", zip: 400053 },
+{ id: 94, fname: "Rahul", lname: "Sharma", age: 28, phonenumber: 7878787878, state: "Maharashtra", city: "Mumbai", zip: 400051 },
+{ id: 95, fname: "Mahesh", lname: "Varma", age: 32, phonenumber: 9797979797, state: "Maharashtra", city: "Mumbai", zip: 400052 },
+{ id: 96, fname: "Jitesh", lname: "Nikam", age: 36, phonenumber: 8787878787, state: "Maharashtra", city: "Mumbai", zip: 400058 },
+{ id: 97, fname: "Hiren", lname: "Patel", age: 27, phonenumber: 9999999999, state: "Maharashtra", city: "Mumbai", zip: 400049 },
+{ id: 98, fname: "Anthony", lname: "Gonsalves", age: 33, phonenumber: 8888888888, state: "Maharashtra", city: "Mumbai", zip: 400045 },]
---
+++ javascript/Elements/employee.js
@@ -0,0 +1,8 @@
+var Users = [{ empid: 91, person : ["Ramesh Singh", 29, 9898989898, "Maharashtra", "Mumbai", 400054] },
+{ empid: 92, person : ["Suresh Patil", 33, 8989898989, "Maharashtra", "Mumbai", 400055] },
+{ empid: 93, person : ["Mahesh Gawade", 35, 7979797979, "Maharashtra", "Mumbai", 400053] },
+{ empid: 94, person : ["Rahul Sharma", 28, 7878787878, "Maharashtra", "Mumbai", 400051] },
+{ empid: 95, person : ["Manish Varma", 32, 9797979797, "Maharashtra", "Mumbai", 400052] },
+{ empid: 96, person : ["Jitesh Nikam", 36, 8787878787, "Maharashtra", "Mumbai", 400058] },
+{ empid: 97, person : ["Hiren Patel", 27, 9999999999, "Maharashtra", "Mumbai", 400049] },
+{ empid: 98, person : ["Anthony Gonsalves", 33, 8888888888, "Maharashtra", "Mumbai", 400045] },]
