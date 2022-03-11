d4f7a6bb4cf69fa3e9a9b104c89c943944d586cf
suryasplucky <suryasplucky@gmail.com>
2022-03-11 14:09:08 +0530

    drop down select

---
+++ Dropdown Select/Dropdown1.js
@@ -0,0 +1,99 @@
+var details = [
+    {
+        friend: "Raju",
+
+        family: [
+            "Raju",
+            "Sai Kiran",
+            "Jaya Sankar",
+            "Sujatha",],
+    },
+    {
+        friend: "Surya",
+        family: [
+            "Surya",
+            "Lokiswari",
+            "Kruthika",
+            "Usha",
+            "Ramesh",
+        ],
+    },
+    {
+        friend: "Niranjan",
+        family: [
+            "nikiniranjan",
+            "nikitha",
+            "kavitha",
+            "manavalan",
+        ],
+    },
+    {
+        friend: "NiKhil kumar",
+        family: [
+            "Nikhil",
+            "Nikitha",
+            "elumalaya",
+            "chinnamma",
+        ],
+    },
+    {
+        friend: "Dileep",
+        family: [
+            "dilleep",
+            "hari",
+            "prakash",
+            "papamma",
+        ],
+    },
+    {
+        friend: "Sandeep",
+        family: [
+            "anjali",
+            "muragayya",
+            "chinna",
+            "sandeep",
+            "chintu",
+        ],
+    },
+    {
+        friend: "Hemadri",
+        family: [
+            "mahesh",
+            "hemadri",
+            "mallika",
+            "nandhini",
+            "somalingam",
+        ],
+
+    },
+    {
+        friend: "swetha",
+        family: [
+            "mohan",
+            "hema",
+            "murali",
+            "nagini",
+            "somayya",
+        ],
+    },
+    {
+        friend: "saranya",
+        family: [
+            "saranya",
+            "lavanya",
+            "madhu",
+            "amma",
+            "nanna",
+        ],
+    },
+    {
+        friend: "girija",
+        family: [
+            "mohani",
+            "hemalatha",
+            "muralikrishna",
+            "nagesh",
+            "somayya",
+        ],
+    },
+];
