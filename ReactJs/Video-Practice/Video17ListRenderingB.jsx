bb3d93413dcae7c2ba0eee42d19214bcc5ef2966
JayasankarRaju <rajubhai7341@.com>
2022-04-06 11:00:47 +0530

    adding

---
+++ ReactJs/Video-Practice/Video17ListRenderingB.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function  Raju (person) {
+    return (
+        <div>
+            <h1>person color is {person.color} person type{person.type} person registration{person.registration}persons Capacity{person.capacity}</h1>
+
+        </div>
+    )
+}
