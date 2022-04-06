e296e47a9c418d5d589670d1746d41469fe16521
Deena <deenav0003@gmail.com>
2022-04-06 18:58:09 +0530

    ...

--- react-JS/List of video-Components/V9-PropsChild.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Person =(props)=>{
-    console.log(props)
-    return (
-        <h2>welcome to{props.MyName} and {props.YourName}</h2>
-    )
-}
-export default  Person
--- react-JS/List of video-Components/V9-PropsParent.jsx
+++
@@ -1,7 +0,0 @@
-import { Component } from "react";
-
-export class Person extends Component{
-    render(){
-    return <h2>HAello{this.props.MyName}at {this.props.YourName}</h2>        
-    }
-}
