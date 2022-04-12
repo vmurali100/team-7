d2c1407d10eecbf0095f6f2140fc5c0f3be3d4fc
JayasankarRaju <rajubhai7341@.com>
2022-04-12 17:35:55 +0530

    LifeCycle-Js

---
+++ ReactJs/LifeCycle/practice1/LifeCycle1.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import Bhai from './Bhai'
+import Raju from './Raju'
+
+export default class LifeCycle extends Component {
+    constructor(props) {
+        console.log(' compnent Constructor')
+      super(props)     
+    }
+
+    
+
+    componentWillMount(){
+        console.log('component WillMount Cycle')
+    }
+    // 2.Udaring
+    shouldComponentUpdate(){
+        console.log('shouldComponentUpdate Cycle')
+        return true
+    }
+    componentDidCatch(){
+        console.log('componentDidCatch Cycle')
+    }
+    componentDidUpdate(){
+        console.log('componentDidUpdate Cycle')
+    }
+   
+  render() {
+    console.log(' component Render Cycle')
+    return (
+      <div>
+          {this .props.myName== "" ? <Bhai/> :< Raju myName={this.props.myName}/>}
+      </div>
+    )
+  }
+  componentDidMount(){
+    console.log('component DidMount Cycle')
+}
+
+  
+}
