82da5bb602c3bc8c3d4545a09f82c2710073097a
vmurali100 <vmurali100@gmail.com>
2022-04-13 22:03:43 +0530

    some

--- ReactJS/app5/src/Components/User.jsx
+++ ReactJS/app5/src/Components/User.jsx
@@ -2,53 +2,6 @@ import { Component } from "react";
 import Hello from "./Hello";
 import Murali from "./Murali";
 
-<<<<<<< HEAD
-export default class User extends Component {
-  constructor(props) {
-    console.log("Component Constructor Cycle");
-    super(props);
-  }
-  // 1. Component Mounting Phase
-
-  componentWillMount() {
-    console.log("componentWillMount Cycle");
-  }
-  // 2. Component Updating Phase
-  shouldComponentUpdate() {
-    console.log("shouldComponentUpdate cycle");
-    return true;
-    // if(this.props.myName == "Murali Krishna"){
-    //     return false
-    // }else{
-    //     return true
-    // }
-  }
-  componentDidCatch() {
-    console.log("componentDidCatch Cycle");
-  }
-  componentDidUpdate() {
-    console.log("componentDidUpdate cycle");
-  }
-
-  // 3. Component Un Mounting Phase
-  render() {
-    console.log("Component Render Cylce");
-    return (
-      <>
-        {this.props.myName == "" ? (
-          <Hello />
-        ) : (
-          <Murali myName={this.props.myName} />
-        )}
-      </>
-    );
-  }
-
-  componentDidMount() {
-    console.log("Component Did Mount Cycle");
-  }
-}
-=======
 export default class User extends Component{
     constructor(props){
         console.log("Component Constructor Cycle")
@@ -92,4 +45,3 @@ export default class User extends Component{
         console.log("Component Did Mount Cycle")
     }
 }
->>>>>>> 1bcbe43047cbd3e7986ad05aa4197df1e74085b1
