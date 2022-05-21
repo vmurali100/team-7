d8c4219b2aeb4d629e9c98eb76c90a53cd7d2c59
Deena <deenav0003@gmail.com>
2022-05-21 04:24:21 -0700

    add

---
+++ Practices/HOComponents/sample-1.jsx
@@ -0,0 +1,33 @@
+import { Component } from "react/cjs/react.production.min";
+
+export const HOComponent =(Empty)=>{
+    class Abc extends Component{
+        constructor(){
+            super()
+            this.state={
+                Money:0
+            }
+        }
+        increament=()=>{
+            this.setState({
+                Money:this.state.Money + 1
+
+            })
+        }
+        Decrement =()=>{
+            this.setState({
+                Money:this.state.Money - 1
+            })
+        }
+        render=() {
+            return <Empty
+            Money={this.state.Money}
+            increament={this.increament}
+            Decrement={this.Decrement}
+            />
+
+        }
+    }
+    return Abc
+}
+export default  HOComponent
